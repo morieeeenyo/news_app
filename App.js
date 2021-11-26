54691f06bd568e1172027c3ae484395c3ddeced2
togo-mentor <uint2435@gmail.com>
2021-11-26 17:36:47 +0900

    stylesオブジェクトから呼び出す

--- App.js
+++ App.js
@@ -6,12 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View
-        style={{
-          height: 100,
-          width: '100%',
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
+        style={styles.itemContainer}
       ></View>
       <StatusBar style="auto" />
     </View>
@@ -25,4 +20,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
 })
